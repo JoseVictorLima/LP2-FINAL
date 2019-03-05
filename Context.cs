dae3c406db3daf3eaac446734191d4b00f84a95c
JoseVictorLima <jsvctr123@gmail.com>
2019-03-05 14:35:51 -0300

    Melhoria do Context

--- Context.cs
+++ Context.cs
@@ -3,8 +3,15 @@ namespace LP2_FINAL
 {
     public class Context: DbContext
     {
+        public DbSet<Funcionario> Funcionarios {get; set;}  
+        public DbSet<Gerente> Gerentes {get; set;}   
+        public DbSet<Operador> Operadores {get; set;}
+        public DbSet<ServicosGerais> ServicosGerais {get; set;}
+        public DbSet<Estoque> Estoques {get; set;}
+        public DbSet<Caixa> Caixas {get; set;}
+        public DbSet<Relatorio> Relatorios {get; set;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)         
-        {             
+        {   
             optionsBuilder.UseSqlite("Data Source=SuperMercadoDataBase.db");         
         }
     }
